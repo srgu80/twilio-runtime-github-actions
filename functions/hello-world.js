47c947cc57302f0f34402f1626a68f492a3bdbdb
srgu80 <111308462+srgu80@users.noreply.github.com>
2022-08-26 14:09:52 +0530

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello World test!"
+        message : "Hello World test1!"
     }
 
     callback(null, result);
